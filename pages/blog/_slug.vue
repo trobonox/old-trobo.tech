33912de45eeba66e9bbe5b993fbf82f2d4f3636c
trobonox <57040351+trobonox@users.noreply.github.com>
2022-05-01 12:52:46 +0200

    Remove unused logging statement

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,8 +1,12 @@
 <template>
   <article>
     <h1 class="text-4xl md:text-6xl font-extrabold mb-2">{{ page.title }}</h1>
-    <h2 class="text-md text-cppnGray-1 mb-2">{{ createdAt }} • {{ page.readingStats.text }}</h2>
-    <p class="text-md md:text-lg mb-10 max-w-none w-full">{{ page.description }}</p>
+    <h2 class="text-md text-cppnGray-1 mb-2">
+      {{ createdAt }} • {{ page.readingStats.text }}
+    </h2>
+    <p class="text-md md:text-lg mb-10 max-w-none w-full">
+      {{ page.description }}
+    </p>
     <nuxt-content
       :document="page"
       class="w-full max-w-4xl mx-0 prose prose-invert prose-img:rounded-sm prose-h1:text-6xl prose-h1:mb-2 prose-h2:mb-2 prose-p:mb-2 prose-p:text-cppnWhite prose-p:text-lg prose-a:text-mauve prose-code:before:content-none prose-code:after:content-none prose-code:bg-cppnBlack-2 prose-code:text-cppnGray-2 prose-code:font-mono prose-code:p-1 prose-code:font-medium prose-blockquote:font-normal"
@@ -16,17 +20,16 @@ export default {
     const page = await $content(params.slug).fetch()
 
     return {
-      page
+      page,
     }
   },
   computed: {
     createdAt() {
-      console.log(this.page)
       const dateparse = Date.parse(this.page.createdAt)
       const dateObject = new Date(dateparse)
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
       return dateObject.toLocaleDateString('en-UK', options)
-    }
+    },
   },
 }
 </script>
@@ -41,7 +44,7 @@ p {
 
 .icon.icon-link {
   @apply inline-block absolute opacity-0 h-6 -ml-6 w-6 bg-no-repeat;
-  background-image: url("@/static/link_icon.svg");
+  background-image: url('@/static/link_icon.svg');
   margin-top: 4px;
 }
 
